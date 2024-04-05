f3b88848e523ec9e2075414a7727cdc2357a11cd
MostafaAlaa297 <elaskary609@gmail.com>
2024-04-04 19:08:51 -0700

    fix code

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,20 +10,16 @@ pack web stack
 
 def do_pack():
     """Generate tgz archive"""
-    try:
         local("mkdir -p versions")
 
         now = datetime.now()
         date_time = now.strftime("%Y%m%d%H%M%S")
-        file_name = "versions/web_static_" + date_time + ".tgz"
-        web_static_path = os.path.abspath("web_static")
+        file_name = "versions/web_static_{}.tgz".format(date_time)
 
-
-        local("tar -cvzf {} {}".format(file_name, web_static_path))
-
-        print("web_static packed {} -> {}Bytes".format(file_name, os.path.getsize(file_name)))
-
-        return file_name
+        result = local("tar -cvzf {} web_static".format(file_name)
+        
+        if result.failed:
+            return None
+        else:
+            return file_name
     
-    except Exception as e:
-        return None
