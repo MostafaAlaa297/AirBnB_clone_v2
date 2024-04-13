c246162e6836af94b2fda05c63f811dab544ae47
MostafaAlaa297 <elaskary609@gmail.com>
2024-04-12 17:29:43 -0700

    fix code

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -12,59 +12,58 @@ env.hosts = ['54.197.130.4', '52.91.160.210']
 
 def do_deploy(archive_path):
     """Distributes an archive to web servers"""
-    if not os.path.isfile(archive_path):
+    if not archive_path:
+        print("False archive_path")
         return False
     try:
         filename = archive_path.split("/")[-1]
         name = filename.split(".")[0]
-        release_path = "/data/web_static/releases/"
-
-        # Upload archive to tmp
-        if put(
-                archive_path, "/tmp/{}".format(filename)).failed is True:
-            return False
+        release_path = "/data/web_static/releases"
+        full_path = "/data/web_static/releases/{}/".format(name)
 
         # Remove dir and achive name if exists
-        if run("rm -rf {}/{}".format(release_path, name)).failed is True:
+        if run("sudo rm -rf {}/{}".format(release_path, name)).failed is True:
+            print("False")
             return False
 
-        # Create directory for extraction
-        if run('mkdir -p {}'.format(
-            "{}/{}").format(
-                release_path, name)).failed is True:
-            return False
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}".format(full_path))
 
         # Uncompress the archive
         if run(
-                'tar -xzf /tmp/{} -C {}/{}'.format(
+                'sudo tar -xzf /tmp/{} -C {}/{}'.format(
                     filename, release_path, name
                     )).failed is True:
+            print("False")
             return False
 
         # Remove the archive
-        if run('rm /tmp/{}'.format(filename)).failed is True:
+        if run('sudo rm /tmp/{}'.format(filename)).failed is True:
+            print("False")
             return False
 
         # Move contents of extracted folder to release folder
         if run(
-                'mv {}/{}/web_static/* {}/{}'.format(
+                'sudo mv {}/{}/web_static/* {}/{}'.format(
                     release_path, name, release_path, name
                     )).failed is True:
+            print("False")
             return False
 
         # remove empty web static folder
         if run(
-                'rm -rf {}/{}/web_static'.format(
+                'sudo rm -rf {}/{}/web_static'.format(
                     release_path, name)).failed is True:
+            print("False")
             return False
 
         # Remove current symlink
-        if run('rm -rf /data/web_static/current').failed is True:
+        if run('sudo rm -rf /data/web_static/current').failed is True:
             return False
 
         # Create new symlink
         if run(
-                'ln -s {}/{}/ /data/web_static/current'.format(
+                'sudo ln -s {}/{}/ /data/web_static/current'.format(
                     release_path, name
                     )).failed is True:
             return False
