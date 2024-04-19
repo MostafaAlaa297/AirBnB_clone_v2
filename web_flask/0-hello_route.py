42a86874ed9171f7e17645ddf00ec8d1d8be7f50
MostafaAlaa297 <elaskary609@gmail.com>
2024-04-19 22:09:26 +0000

    fix code

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
-"""displays Hello HBNB!"""
+	"""displays Hello HBNB"""
 	return 'Hello HBNB!'
 
 if __name__ ==  '__main__':
